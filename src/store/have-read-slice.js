21ad7a7da0e6117eb8767e082ae22c71803da4b7
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-03-16 13:59:56 +0530

    Styles changes

---
+++ src/store/have-read-slice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const ToReadSlice = createSlice({
+  name: "To-Read",
+  initialState: {
+    completedBooks: [],
+  },
+  reducers: {
+    addToCompleted(state, action) {
+      const newBook = action.payload;
+      const isExist = state.completedBooks.find(
+        (item) => item.id === newBook.id
+      );
+
+      if (!isExist) {
+        state.completedBooks.push({
+          id: newBook.id,
+          image: newBook.image,
+          title: newBook.title,
+          authors: newBook.authors, //aray
+          categories: newBook.categories, //aray
+          language: newBook.language,
+          pages: newBook.pages,
+          description: newBook.description,
+        });
+      }
+    },
+  },
+  removeFromCompleted(state, action) {
+    const id = action.payload;
+    state.completedBooks = state.completedBooks.filter(
+      (item) => item.id !== id
+    );
+  },
+});
---
+++ src/store/to-read-slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const ToReadSlice = createSlice({
+  name: "To-Read",
+  initialState: {
+    ToReadBooks: [],
+  },
+  reducers: {
+    addToToRead(state, action) {
+      const newBook = action.payload;
+      const isExist = state.ToReadBooks.find((item) => item.id === newBook.id);
+
+      if (!isExist) {
+        state.ToReadBooks.push({
+          id: newBook.id,
+          image: newBook.image,
+          title: newBook.title,
+          authors: newBook.authors, //aray
+          categories: newBook.categories, //aray
+          language: newBook.language,
+          pages: newBook.pages,
+          description: newBook.description,
+        });
+      }
+    },
+  },
+  removeFromToRead(state, action) {
+    const id = action.payload;
+    state.ToReadBooks = state.ToReadBooks.filter((item) => item.id !== id);
+  },
+});
