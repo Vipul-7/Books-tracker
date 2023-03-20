28934ceb96c1796d920c46c7fcca19ae89bb58ce
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-03-20 14:29:16 +0530

    secure the api keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOtUUvw3phs05R2YJYVE9nVDJauBxPipM",
-  authDomain: "book-tracker--api.firebaseapp.com",
-  projectId: "book-tracker--api",
-  storageBucket: "book-tracker--api.appspot.com",
-  messagingSenderId: "443989530920",
-  appId: "1:443989530920:web:b944828a509271fc0e981d",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
